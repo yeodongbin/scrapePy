e0af84bb07d5005d577d62b8ffa6129fe3ad3737
Administrator <Administrator@WIN-GST8L6FGFJT>
2018-06-29 10:48:47 +0900

    2018 06 29

---
+++ autoExcel/extractMain.py
@@ -0,0 +1,1 @@
+from extractSurveyContents import ControlExcel as CE
---
+++ autoExcel/extractSurveyContents.py
@@ -0,0 +1,18 @@
+class ControlExcel:
+    def __init__(self):
+        print()
+
+    def open_excel(self, file_name):
+        pass
+
+    def make_new_excel_file(self,file_name):
+        pass
+
+    def move_contents_from(self, source_file, created_file):
+        pass
+
+    def find_excel_file(self, directory_name):
+        pass
+
+if __name__ == '__main__':
+    print()
