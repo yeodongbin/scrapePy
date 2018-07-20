83856b20c8223145b4c441b408dd011930a25820
yeodongbin <yeodongbin2011@gmail.com>
2018-07-20 12:35:00 +0900

    2018 07 20 사람인 인재검색 완료

--- scrape/scrapeJobKorea.py
+++ scrape/scrapeJobKorea.py
@@ -1,83 +1,97 @@
+# pip install selenium
+# pip install bs4
+# 팬텀JS http://phantomjs.org/download.html
+# pip install pymysql #데이터 베이스
+# from selenium.webdriver.common.by import By
+# 명시적 대기를 위해
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 
 from selenium import webdriver as wd
-from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup as bs
+import time
 import sys
 
-#함수 정의
-def getLinks(source):
-    websiteLinks = []
-    for link in source.find_all('a'):
-        url = link.get('href')
-        if url:
-            websiteLinks.append(url)
-
-    for link in websiteLinks:
-        print(link)
-
-    return websiteLinks
-
 # 사전에 필요한 정보를 로드
-main_url = 'http://www.jobkorea.co.kr/Corp/Main'
-search_url = 'http://www.jobkorea.co.kr/Corp/Person/FindByKey?key=bkFZGbH5eiM5qhQXmKw0GBV_QPacryptoMS7TmyjYuX8sjMoNkQS2fNRiIWQeBXFe5Fpf'
-#platform_develop_url = 'http://www.gamejob.co.kr/List_GG/GG_Part_Search.asp?Part_Code=12&car_u=0&car_d=1&age_u=1999&age_d=1988&S_Keyword='
-id = "kitriis"
-password = "kitriis0908"
-file_name = "JobKorea.txt"
-total_num = 0
+main_url = 'http://www.saramin.co.kr/zf_user/'
+search_url = 'http://www.saramin.co.kr/zf_user/talent/search'
+id = "kitri"
+password = "kitri0908"
+file_name = "Saramin.txt"
+ELEMENT_COUNT_PER_PAGE = 20
+PAGE_COUNT = 10
+start_age = 26
+end_age = 28
 
 # 드라이버 로드 # 윈도우용
 driver = wd.Chrome(executable_path='chromedriver.exe')
 
-# 사이트 접속 (get)
+# 사이트 접속, 로그인
 driver.get(main_url)
 driver.implicitly_wait(2)
-driver.find_element_by_name('M_ID').send_keys(id)
-driver.find_element_by_name('M_PWD').send_keys(password)
-driver.find_element_by_class_name('btnLogin').click()
+driver.find_element_by_id('btn_tab_com').click()
+driver.find_element_by_name('id').send_keys(id)
+driver.find_element_by_name('password').send_keys(password)
+driver.find_element_by_id('login_btn').click()
 driver.implicitly_wait(2)
+
 driver.get(search_url)
+driver.find_element_by_class_name('btn_box_close').click() #팝업없애기
+driver.find_element_by_id('btn-display-save-list').click() #저장된 설정 가져오기
 driver.implicitly_wait(2)
 
+save_option_area = driver.find_element_by_id('save_option_area')
+link_element = save_option_area.find_element_by_class_name('title')
+link_element.click()
+driver.implicitly_wait(2)
 
-# 페이지 이동
-# pages = ['2','3']
-# for page in pages:
-#     print(type(page))
-#     driver.find_element_by_partial_link_text(page).click()
-#     driver.implicitly_wait(2)
-# <a href="#" data-page="2">2</a>
-# <a href="#" data-page="2">2</a>
-# <a href="#" data-page="3">3</a>
-#dvBasicResumeList > section > div.tplPagination.js-ListPaging > ul > li:nth-child(3) > a
-#//*[@id="dvBasicResumeList"]/section/div[2]/ul/li[2]/a
+# 나이 입력
+driver.find_element_by_xpath('//*[@id="max-age"]/option[10]').click()#26세
+driver.find_element_by_xpath('//*[@id="min-age"]/option[4]').click() #28세
+time.sleep(2) #페이지 이동간 대기
+
+#Element 확인 (검색된 전체 인원 파악)
+element_count = int(driver.find_element_by_id('resumeCnt').text.replace(',',''))
+page_count = element_count // ELEMENT_COUNT_PER_PAGE # TOTAL PAGE COUNT
+list_count = element_count // (ELEMENT_COUNT_PER_PAGE*PAGE_COUNT)
+
+print('-------------------------------------------------------')
+print(element_count, type(element_count))
+print(page_count)
+print(list_count)
+print('-------------------------------------------------------')
+
+f = open(file_name,'w')
+count = 0;
+
+# 페이지 이동하는 코드
+count = 1
+for next_count in range(1,2): #next 버튼
+    pages_url = driver.find_elements_by_class_name("page")
+    print(len(pages_url))
+
+    for page_url in pages_url:
+        print(len(pages_url))
+        page_url.click()
+        time.sleep(2)  # 페이지 이동간 대기
+
+        #각 타이틀별로 url 이동
+        current_url = driver.current_url #현재 url 저장
+        people_title = []
+        people_url = []
+        people_title = driver.find_elements_by_xpath("//a[@class = 'title resumeSubject']")
+
+        driver.get(current_url) #검색된 페이지로 이동
+        driver.find_element_by_class_name('btn_box_close').click()  # 팝업없애기
+
+    print("%s장 ok" % count)
+    next_button = driver.find_elements_by_class_name('btn_next')
+    next_button[1].click()
+    time.sleep(3)
+#__________________________________________________________________
+f.close()
+driver.close()
+driver.quit()
+sys.exit()
 
 
-#개인 정보 취합
-
-#
-# p_url = driver.find_element_by_id('linkResume2')
-# url = p_url.find_element_by_css_selector('a').get_attribute('href')
-# #people_id = driver.find_elements_by_id('linkResume2').get_attribute('href')
-# print(url)
-#print(people_id)
-i = 0
-# for p_url in people_url:
-#     i += 1
-#
-#     # Keys.TAB
-#     # Keys.TAB
-#     # Keys.TAB
-#     # Keys.ENTER
-#     print(url)
-
-
-
-
-# for page in range(2, 9):
-#     try :
-#         driver.execute_script("Javascript:data-page(this.form,%s)" % page)
-#     except Exception as e:
-#         print('오류 e ', e)
-
-#driver.quit()
--- scrape/scrapeSaramin.py
+++ scrape/scrapeSaramin.py
@@ -20,11 +20,13 @@ password = "kitri0908"
 file_name = "Saramin.txt"
 ELEMENT_COUNT_PER_PAGE = 20
 PAGE_COUNT = 10
+start_age = 26
+end_age = 28
 
 # 드라이버 로드 # 윈도우용
 driver = wd.Chrome(executable_path='chromedriver.exe')
 
-# 사이트 접속 (get)
+# 사이트 접속, 로그인
 driver.get(main_url)
 driver.implicitly_wait(2)
 driver.find_element_by_id('btn_tab_com').click()
@@ -32,124 +34,105 @@ driver.find_element_by_name('id').send_keys(id)
 driver.find_element_by_name('password').send_keys(password)
 driver.find_element_by_id('login_btn').click()
 driver.implicitly_wait(2)
+
 driver.get(search_url)
-driver.find_element_by_class_name('btn_box_close').click()
-driver.find_element_by_id('btn-display-save-list').click()
+driver.find_element_by_class_name('btn_box_close').click() #팝업없애기
+driver.find_element_by_id('btn-display-save-list').click() #저장된 설정 가져오기
 driver.implicitly_wait(2)
+
 save_option_area = driver.find_element_by_id('save_option_area')
-print(type(save_option_area))
 link_element = save_option_area.find_element_by_class_name('title')
-print(type(link_element))
 link_element.click()
 driver.implicitly_wait(2)
+
+# 나이 입력
+driver.find_element_by_xpath('//*[@id="max-age"]/option[10]').click()#26세
+driver.find_element_by_xpath('//*[@id="min-age"]/option[4]').click() #28세
 time.sleep(2) #페이지 이동간 대기
 
+#Element 확인 (검색된 전체 인원 파악)
 element_count = int(driver.find_element_by_id('resumeCnt').text.replace(',',''))
 page_count = element_count // ELEMENT_COUNT_PER_PAGE # TOTAL PAGE COUNT
 list_count = element_count // (ELEMENT_COUNT_PER_PAGE*PAGE_COUNT)
 
-#각 타이틀별로 url 이동
-people_url = []
-people_title = driver.find_elements_by_xpath("//a[@class = 'title resumeSubject']")
-for p_title in people_title:
-    try:
-        p_url = p_title.find_element_by_css_selector('a').get_attribute('href')
-        people_url.append(p_url)
-    except Exception as e1:
-        print('오류', e1)
+print('-------------------------------------------------------')
+print(element_count, type(element_count))
+print(page_count)
+print(list_count)
+print('-------------------------------------------------------')
 
-count = 0
-for p_url in people_url:
-    count += 1
-    print(count, p_url)
-    driver.get(p_url)
-    driver.implicitly_wait(2)
-    soup = bs(driver.page_source, 'html.parser')
+f = open(file_name,'w')
+count = 0;
 
 # 페이지 이동하는 코드
-# for list_number in range(1, list_count):
-#     print('PAGE NUMBER', list_number)
-#     count = 1
-#     pages_url = driver.find_elements_by_class_name('page')
-#     btn_next_url = driver.find_elements_by_class_name('btn_next')
-#     for page_url in pages_url:
-#         page_url.click()  # page_url로 이동하기
-#         count = count+1
-#         driver.implicitly_wait(2)
-#         if ((count % PAGE_COUNT) == 0):
-#             btn_next_url[1].click()
-#             time.sleep(2)
+count = 1
+for next_count in range(1,2): #next 버튼
+    pages_url = driver.find_elements_by_class_name("page")
+    print(len(pages_url))
+
+    for index in range(1,10):
+        pages_url = driver.find_elements_by_class_name("page")
+        print(len(pages_url))
+        pages_url[index].click()
+        time.sleep(2)  # 페이지 이동간 대기
+
+        #각 타이틀별로 url 이동
+        current_url = driver.current_url #현재 url 저장
+        people_title = []
+        people_url = []
+        people_title = driver.find_elements_by_xpath("//a[@class = 'title resumeSubject']")
+
+        #각 타이틀의 URL 저장 (20개씩)
+        for p_title in people_title:
+            try:
+                p_url = p_title.get_attribute('href')
+                people_url.append(p_url)
+            except Exception as e2:
+                print('오류', e2)
+
+        #각 타이틀의 URL로 이동
+        for p_url in people_url:
+            try:
+                driver.get(p_url) #
+                obscured_element = None
+                time.sleep(2)  # 페이지 이동간 대기
+
+                #가려진 항목 선택
+                try:
+                    obscured_element = driver.find_element_by_xpath('// *[ @ id = "resume_print_area"] / div / div[1] / div[2] / div[2] / ul / li[1] / a')
+                except Exception as e:
+                    pass
+
+                if (obscured_element):
+                    obscured_element.click()
+                    time.sleep(2)
+
+                p_info = driver.find_element_by_class_name('myname').text
+                phone = driver.find_element_by_class_name('phone').text
+                email = driver.find_element_by_class_name('mail').text
+                print(p_info[:3],phone[:14],email)
+
+                # 파일 입력
+                if email!='비공개':
+                    f.write(str(count) + ",")
+                    f.write(p_info[:3] + ",")
+                    f.write(phone[:14] + ",")
+                    f.write(email + "\n")
+                    count += 1
+            except Exception as e1:
+                print('오류')
+                continue
+        driver.get(current_url) #검색된 페이지로 이동
+        driver.find_element_by_class_name('btn_box_close').click()  # 팝업없애기
+
+    print("%s장 ok" % count)
+    next_button = driver.find_elements_by_class_name('btn_next')
+    next_button[0].click()
+    time.sleep(3)
+#__________________________________________________________________
+f.close()
+driver.close()
+driver.quit()
+sys.exit()
 
 
-#페이지 담기
-#     try:
-#         driver.find_elements_by_xpath("//button[@class = 'page'][@value='%s']" % page)
-#         driver.get(search_url)
-#         driver.execute_script("Javascript:go_db_page(this.form,%s)" % page)
-#         driver.implicitly_wait(2)
-#         print("\n********** %s 페이지 이동 **********\n" % page)
-#         people_url = []
-#
-#         people_title = driver.find_elements_by_xpath("//td[@class = 'p_list']")
-#         #print(len(people_title), type(people_title))
-#
-#         if (len(people_title))==22:
-#             del people_title[0:2]
-#             print(len(people_title), type(people_title))
-#
-#         #개인 url 추출
-#         for p_title in people_title:
-#             try:
-#                 p_url = p_title.find_element_by_css_selector('a').get_attribute('href')
-#                 people_url.append(p_url)
-#             except Exception as e1:
-#                 print('오류',e1)
-#
-#         #url에 접속해 필요 정보 추출(Email, 이름)
-#         for p_url in people_url:
-#             try:
-#                 count += 1
-#                 print(count, p_url)
-#
-#                 try:
-#                     driver.get(p_url)
-#                     driver.implicitly_wait(2)
-#                     soup = bs(driver.page_source, 'html.parser')
-#                 except Exception as e3:# 접속 불가시에 예외처리
-#                     alrt = driver.switch_to_alert()#popup close
-#                     alrt.accept()
-#                     print('오류 e3 ', e3)
-#                     continue
-#
-#                 #name , email 추출
-#                 name_list = soup.findAll("font",{"class":"b c_skyblue"})
-#                 name = name_list[0].get_text()
-#                 print('Name : ', name)
-#
-#                 email_list = soup.findAll("font", {"class": "c_blue"})
-#                 if len(email_list)>=1:# email 없을 경우 예외처리
-#                     email = email_list[0].get_text()
-#                     print('E-Mail : ',email)
-#                 else:
-#                     email = "E-Mail 없음"
-#                     print("E-Mail 없음")
-#
-#                 #파일 입력
-#                 f.write(str(count)+",")
-#                 f.write(name[0:3]+",")
-#                 f.write(email+"\n")
-#             except Exception as e2:
-#                 print('오류 e2 ', e2)
-#                 break
-#     except Exception as e:
-#         print('오류 e ', e)
-
-# 파일 오픈
-# f = open(file_name,'w')
-# # count = 0
-
-# 종료
-# # f.close()
-# driver.close()
-# driver.quit()
-# sys.exit()
